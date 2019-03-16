6d092b307fa958f4e516f530ac5a671ed026352c
Tower-7 <982155153@qq.com>
2019-03-16 13:42:35 +0800

    frist commit

---
+++ server/Controllers/move.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //主页
+    index: async ctx => {
+        await ctx.render('move/index',{
+            
+        })
+    },
+}
---
+++ server/router.js
@@ -0,0 +1,10 @@
+const Router = require('koa-router')
+const Move = require('./Controllers/move')
+const router = new Router()
+
+module.exports = (app) => {
+    //移动端路由
+    router.get('/m',Move.index) //移动主页
+
+    app.use(router.routes()).use(router.allowedMethods())
+}
