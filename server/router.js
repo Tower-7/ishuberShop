85fab6f13e8e9a83196a2d669dc32f06c57fc6b2
Tower-7 <982155153@qq.com>
2019-03-20 14:50:33 +0800

    first commit

--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ const router = new Router()
 module.exports = (app) => {
     //移动端路由
     router.get('/m',Move.index) //移动主页
-    router.get('/detail',Move.detail) //移动主页
+    router.get('/m/detail',Move.detail) //移动主页
 
     app.use(router.routes()).use(router.allowedMethods())
 }
